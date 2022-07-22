03aacf50ac6b105ed9e338059da006a84513e557
oh in seong <58154638+ois0886@users.noreply.github.com>
2022-07-23 01:41:17 +0900

    백준알고리즘 2941번 크로아티아 알파벳

--- 2941_크로아티아알파벳.py
+++
@@ -1,9 +0,0 @@
-n = input()
-d = ['c=','c-','dz=','d-','lj','nj','s=','z=']
-
-cnt = 0
-for i in d:
-    while i in n:
-        cnt += n.count(i)
-        n = n.replace(i,'0'*len(i))
-print(cnt + len(n) - n.count('0'))
